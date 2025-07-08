c58e02b881819ab34d899911952572cdea421aa9
Itaifos <itaifos@gmail.com>
2025-07-08 00:08:07 -0300

    :recycle: refactor: ajuste conexao spotify

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -1,10 +1,11 @@
+// src/util/spotify.js
 import axios from 'axios'
 import { encode as btoa } from 'base-64'
-import { YOUR_REACT_NATIVE_SPOTIFY_CLIENTID, YOUR_REACT_NATIVE_SPOTIFY_ANON_SECRET } from './keys'
+import { SPOTIFY_CLIENTID, SPOTIFY_ANON_SECRET, SPOTIFY_REFRESH_TOKEN } from './keys'
 
-const CLIENT_ID = YOUR_REACT_NATIVE_SPOTIFY_CLIENTID
-const CLIENT_SECRET = YOUR_REACT_NATIVE_SPOTIFY_ANON_SECRET
-
+const CLIENT_ID = SPOTIFY_CLIENTID
+const CLIENT_SECRET = SPOTIFY_ANON_SECRET
+const REFRESH_TOKEN = SPOTIFY_REFRESH_TOKEN
 
 let accessToken = ''
 let tokenExpiresAt = 0
@@ -16,23 +17,35 @@ export async function getAccessToken() {
     return accessToken
   }
 
-  const authString = `${CLIENT_ID}:${CLIENT_SECRET}`
-  const base64Auth = btoa(authString)
+  try {
+    const base64Auth = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
 
-  const response = await axios.post(
-    'https://accounts.spotify.com/api/token',
-    'grant_type=client_credentials',
-    {
-      headers: {
-        Authorization: `Basic ${base64Auth}`,
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    }
-  )
+    const response = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: REFRESH_TOKEN,
+      }).toString(),
+      {
+        headers: {
+          Authorization: `Basic ${base64Auth}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
 
-  const { access_token, expires_in } = response.data
-  accessToken = access_token
-  tokenExpiresAt = now + expires_in * 1000
+    const { access_token, expires_in } = response.data
+    accessToken = access_token
+    tokenExpiresAt = now + expires_in * 1000
 
-  return accessToken
+    console.log('🎧 Novo token gerado com refresh_token')
+    console.log(accessToken)
+    return accessToken
+  } catch (error) {
+      console.error('❌ Erro ao obter access_token do Spotify:')
+      console.error('Status:', error.response?.status)
+      console.error('Data:', error.response?.data)
+      console.error('Mensagem:', error.message)
+    throw error
+  }
 }
